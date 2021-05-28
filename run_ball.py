90eba0fe25f8d16d9246df71ee661e6cfcb06217
Elyssana <53406214+Elyssana@users.noreply.github.com>
2021-05-27 23:26:08 -0300

    Revert "Merge branch 'main' of https://github.com/nothellena/projeto-redes into main"
    This reverts commit 934705cc7d92f8e6dc24da61171fc1ceb286c045, reversing
    changes made to fa3ff4dbce204ce08c92781b59b59e7d19ea68f8.

--- run_ball.py
+++ run_ball.py
@@ -4,11 +4,13 @@ from utils import *
 from ball import Ball
 from player import Player
 
+
 BG = 40, 42, 54
 GREEN = 80, 250, 123
 
 def redrawWindow(win, players):
     win.fill(BG)
+
 
     for player in players:
         player.draw(win)
@@ -16,40 +18,34 @@ def redrawWindow(win, players):
     pygame.display.update()
 
 
-def main():
+def run_b():
+
     run = True
     n = Network()
 
-    x, y, aux1, aux2 = read_single_object_info(n.getPos())
+    x, y, h, w, color, type = read_single_object_info(n.getPos())
 
-    ball = Ball(x, y, GREEN, 10, 10)
-
-    players = [Player(x, y, 10, 50, BG, 'p'), Player(x, y, 10, 50, BG, 'p'),
-               Player(x, y, 10, 50, BG, 'p'), Player(x, y, 10, 50, BG, 'p')]
-
+    p = Ball(x,y,GREEN,w,h)
+    other_players = [Player(x, y, w, h, BG, type),Player(x, y, w, h, BG, type),
+                     Player(x, y, w, h, BG, type),Player(x, y, w, h, BG, type)]
     clock = pygame.time.Clock()
 
     while run:
 
-        objects = read_objects_info(n.send(create_single_object_info(ball)))
-        attributes = [['x', 0], ['y', 1], ['color', 2]]
+        objects = read_objects_info(n.send(create_single_object_info(p)))
+        attributes = [['x', 0], ['y', 1], ['width', 3], ['height', 2], ['color', 4], ['type', 5]]
         j = 0
-
-        if ball.scoreB >= 10 or ball.scoreA >= 10:
-            run = False
-            pygame.quit()
 
         for obj in objects:
             for atr, i in attributes:
-                setattr(players[j], atr, obj[i])
-            players[j].update()
+                setattr(other_players[j], atr, obj[i])
+            other_players[j].update()
             j += 1
 
-        for player in players:
-            ball.check_collision(player)
+        for player in other_players:
+            p.check_collision(player)
 
-        ball.move()
+        p.move()
         clock.tick(60)
 
-
-main()
+run_b()
--- utils.py
+++ utils.py
@@ -1,9 +1,9 @@
 def get_color(string):
-    s = string.replace("(", "").replace(")", "")
+
+    s = string.replace("(","").replace(")","")
     s = s.split(",")
 
-    return (int(s[0]), int(s[1]), int(s[2]))
-
+    return (int(s[0]),int(s[1]),int(s[2]))
 
 def create_objects_info(objects):
     i = 0
@@ -20,25 +20,21 @@ def create_objects_info(objects):
 
 
 def create_single_object_info(obj):
-    if obj.type == 'b':
-        return str(int(obj.x)) + "-" + str(int(obj.y)) + "-" + str(obj.scoreA) + "-" + str(obj.scoreB)
 
-    return str(int(obj.x)) + "-" + str(int(obj.y)) + "-" + str(obj.color) + "-" + "p"
+    if obj.type == 'b':
+        return str(int(obj.x)) + "-" + str(int(obj.y)) + "-" + str(obj.scoreA) + "-" + str(obj.scoreB) + "-" + "(80, 250, 123)" + "-"+ "None"
+
+    return str(int(obj.x)) + "-" + str(int(obj.y)) + "-" + str(int(obj.height)) + "-" + str(int(obj.width)) + "-" + str(obj.color) + "-" + str(obj.type)
 
 
 def read_single_object_info(data):
     info_list = data.split("-")
     x, y = int(info_list[0]), int(info_list[1])
-    last = info_list[3]
+    height, width = int(info_list[2]), int(info_list[3])
+    color = get_color(info_list[4])
+    type = info_list[5]
 
-    if last == 'p':
-        data1 = get_color(info_list[2])
-        data2 = last
-    else:
-        data1 = int(info_list[2])
-        data2 = int(last)
-
-    return x, y, data1, data2
+    return x, y, height, width, color, type
 
 
 def read_objects_info(data):
@@ -46,7 +42,7 @@ def read_objects_info(data):
     objects = []
 
     for obj in objects_info:
-        x, y, data1, data2 = read_single_object_info(obj)
-        objects.append([x, y, data1, data2])
+        x, y, height, width, color, type = read_single_object_info(obj)
+        objects.append([x, y, height, width, color, type])
 
     return objects
